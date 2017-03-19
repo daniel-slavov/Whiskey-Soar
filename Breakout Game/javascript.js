70893d64b7febee7367843bd1ce325ed486803e8
daniel-slavov <daniel.slavov@aol.com>
2017-03-19 15:21:09 +0200

    update
    update

--- Breakout Game/javascript.js
+++ Breakout Game/javascript.js
@@ -1,8 +1,24 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
+context.translate(0.5, 0.5);
 
-context.beginPath();
-context.rect(20, 40, 50, 50);
-context.fillStyle = "#FF0000";
-context.fill();
-context.closePath();
+function draw() {
+    drawBall();
+}
+setInterval(draw, 10);
+
+var canvasWidth = canvas.width;
+canvas.heigth = canvas.width * 0.75;
+
+function drawBall() {
+    const ballRadius = 0.01 * canvasWidth;
+    context.beginPath();
+    context.arc(50, 50, 10, 0, Math.PI*2);
+    context.fillStyle = "#0095DD";
+    context.fill();
+    context.closePath();
+}
+
+function drawPaddle() {
+    
+}
