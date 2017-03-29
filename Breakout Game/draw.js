f14a57de10422833fd1d08cb31defd57a6b36f73
Yordan Dimitrov <dadanchi@abv.bg>
2017-03-29 16:56:59 +0300

    Separated the javascript in 3 files

--- Breakout Game/draw.js
+++ Breakout Game/draw.js
@@ -1,83 +1,3 @@
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-context.translate(0.5, 0.5);
-
-var ballRadius = 0.01 * canvas.width;
-var x;
-var y;
-var dx;
-var dy;
-var actualSpeed;
-var paddleHeight;
-var paddleWidth;
-var paddleX;
-var paddleY;
-var rightPressed = false;
-var leftPressed = false;
-var isPause = false;
-
-var brickRowCount = 5;
-var brickColumnCount = 8;
-var brickWidth;
-var brickHeight;
-var brickSpacing;
-var brickClusterPadding;
-var brickClusterWidth;
-var brickClusterPaddingToCanvasWidthRatio = 0.05;
-var brickHeightToWidthRatio = 0.25;
-var brickSpacingToWidthRatio = 0.1;
-
-// Init bricks
-var bricks = [];
-for (col = 0; col < brickColumnCount; col += 1) {
-    bricks[col] = [];
-    for (row = 0; row < brickRowCount; row += 1) {
-        if (row % 2 === 0) {
-            bricks[col][row] = { x: 0, y: 0, status: 1 };
-        } else {
-            bricks[col][row] = { x: 0, y: 0, status: 2 };
-        }
-
-    }
-}
-
-var score = 0;
-const maxPoints = 560;
-var lives = 3;
-var windowHeight = window.innerHeight;
-var windowWidth = window.innerWidth;
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("mousemove", mouseMoveHandler, false);
-
-function keyDownHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = true;
-    } else if (e.keyCode == 37) {
-        leftPressed = true;
-    } else if (e.keyCode === 32) {
-        isPause = !isPause;
-    }
-}
-
-function keyUpHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = false;
-    } else if (e.keyCode == 37) {
-        leftPressed = false;
-    }
-}
-//Allow moving the paddle with the mouse
-function mouseMoveHandler(e) {
-    var mousePosition = e.clientX,
-        distanceToCurrentWindow = canvas.offsetLeft,
-        currentPosition = mousePosition - distanceToCurrentWindow;
-    if (currentPosition > 0 && currentPosition < canvas.width) {
-        paddleX = currentPosition - paddleWidth / 2;
-    }
-}
-
 function drawBall() {
     context.beginPath();
     context.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -195,13 +115,7 @@ function collisionDetection() {
     }
 }
 
-function movePaddle() {
-    if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += 7;
-    } else if (leftPressed && paddleX > 0) {
-        paddleX -= 7;
-    }
-}
+
 
 function ballIsInRange() {
     // right & left check
@@ -316,6 +230,7 @@ function resizeCanvas() {
     y = paddleY - ballRadius;
 
     // Bricks
+
     brickClusterPadding = brickClusterPaddingToCanvasWidthRatio * canvas.width;
     brickClusterWidth = canvas.width - (2 * brickClusterPadding);
     brickSpacing = brickClusterWidth / (((brickColumnCount) * (1 / brickSpacingToWidthRatio)) + (brickColumnCount - 1));
@@ -334,10 +249,15 @@ function startSoundTrack() {
     audio.play();
 }
 
+function update() {
+    x += dx;
+    y += dy;
+}
+
 function draw() {
     if (!isPause) {
-        context.clearRect(0, 0, canvas.width, canvas.height);
         checkWindowSize();
+        context.clearRect(0, 0, canvas.width, canvas.height);
         drawBricks();
         drawBall();
         drawPaddle();
@@ -347,9 +267,8 @@ function draw() {
         ballIsInRange();
         movePaddle();
         startSoundTrack();
+        update();
 
-        x += dx;
-        y += dy;
     }
 }
 
